817624ef664d13fed47d246754c6afdcbd4a9e2e
Giuseppe Giordano <58441616+Peppino89@users.noreply.github.com>
2026-05-09 11:09:24 +0200

    Modifica classe Admin

--- ProgettoGestioneTicket/src/model/Admin.java
+++ ProgettoGestioneTicket/src/model/Admin.java
@@ -1,7 +1,14 @@
 package model;
 
-public class Admin extends Utente {
-    public Admin(String nomeUtente, String password) {
+public class Admin extends UtenteObserver{
+
+    public Admin(String nomeUtente, String password){
         super(nomeUtente,password,"ADMIN");
     }
+
+    @Override
+    public void update(String messaggio) {
+        System.out.println("[NOTIFICA ADMIN "+super.getNomeUtente()+"] "+messaggio);
+    }
 }
+
